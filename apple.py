d540e96d1657390806babf4c6847e7e376274137
Mateusz Serek <mateuszserek@192.168.1.5>
2024-12-25 13:14:21 +0100

    added sound after eating 5 * x apples

--- apple.py
+++ apple.py
@@ -1,11 +1,11 @@
 from random import randint, choice
 import config 
-# import simpleaudio as sa
-
-# def play_music():
-#     wave_obj = sa.WaveObject.from_wave_file("audio/div5.wav")
-#     play_obj = wave_obj.play() 
-#     play_obj.wait_done() 
+import pygame
+#do zrobienia bedzie konfiguracja venv jak bede oddawal projekt
+def play_music(): #dziala, za 1 razem jest maly lag, wiec trzeba to zainicjowac na poczatku programu
+    pygame.mixer.init()
+    pygame.mixer.music.load("audio/div5.wav")
+    pygame.mixer.music.play()
 
 def generate_pos():
     return [randint(config.game_screen_x_start + 1, config.screen_x_size - 1), randint(config.game_screen_y_start + 1, config.screen_y_size - 1)]
@@ -20,4 +20,4 @@ class Apple:
         self.position = generate_pos()
         if self.counter % 5 == 0 and config.game_tick_ms > 15:
             config.game_tick_ms -= 5
-            #play_music() - po skonczeniu dzwieku rozwala caly program
+            play_music()
